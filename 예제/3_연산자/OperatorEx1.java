c03691de2721ce4e64a43a72739869616c4ee28a
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-07-18 23:46:33 +0900

    OperatorEx1

---
+++ 예제/3_연산자/OperatorEx1.java
@@ -0,0 +1,11 @@
+public class OperatorEx1 {
+    public static void main(String[] args) {
+        int i = 5;
+        i++; // i = i + 1;과 같은 의미, ++i를 써도 결과는 같다
+        System.out.println(i);
+
+        i = 5;
+        ++i;
+        System.out.println(i);
+    }
+}
