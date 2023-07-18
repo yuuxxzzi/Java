97b45ae8fd827ba4dc26d64940eecba5d5d56a56
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-07-18 23:54:11 +0900

    ScannerEx

---
+++ 예제/2_변수/ScannerEx.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class ScannerEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("두자리 정수를 하나 입력해주세요. > ");
+        String input = scanner.nextLine();
+        int num = Integer.parseInt(input); // 입력받은 문자열을 숫자로 변환
+
+        System.out.println("입력 내용 : " + input);
+        System.out.printf("num = %d %n", num);
+    }
+}
