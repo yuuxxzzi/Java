e6782b961f35d77c1b8a394cd2ad14e693b6238e
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-07-19 00:22:59 +0900

    SpecialCharEx

---
+++ 예제/2_변수/SpecialCharEx.java
@@ -0,0 +1,9 @@
+public class SpecialCharEx {
+    public static void main(String[] args) {
+        System.out.println('\''); // ''' 처럼 적으면 안됨
+        System.out.println("abc\t\123\b 456");
+        System.out.println('\n');
+        System.out.println("\"Hello\""); // " 출력방법
+        System.out.println("c:\\"); // \ 출력
+    }
+}
