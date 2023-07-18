dd7c529a5166b5b93d84e814f54124a28afbb6fa
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-07-19 00:30:28 +0900

    OverflowEx

---
+++ 예제/2_변수/OverflowEx.java
@@ -0,0 +1,18 @@
+public class OverflowEx {
+    public static void main(String[] args) {
+        short sMin = -32768;
+        short sMax = 32767;
+        char cMin = 0;
+        char cMax = 65535;
+
+        System.out.println("sMin = "+ sMin);
+        System.out.println("sMin - 1 = "+ (short)(sMin-1));
+        System.out.println("sMax = "+ sMax);
+        System.out.println("sMax + 1 = "+ (short)(sMax + 1));
+        System.out.println("cMin = "+ (int)sMin);
+        System.out.println("cMin - 1 = "+ (int)--cMin);
+        System.out.println("cMax = "+ (int)sMax);
+        System.out.println("cMax + 1 = "+ (int)++cMin);
+
+    }
+}
