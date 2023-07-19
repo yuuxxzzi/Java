a894047e872c61c1ca6fb709c105ab902450cb6f
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-07-19 15:53:54 +0900

    OperatorEx5

--- 예제/3_연산자/OperatorEx5.java
+++ 예제/3_연산자/OperatorEx5.java
@@ -10,6 +10,7 @@ public class OperatorEx5 {
         System.out.printf("%d / %f = %f%n", a, (float)b, a /(float)b);
 
         // 정수끼리 0으로 나누면 오류가 발생하지만, 실수형끼리는 0으로 나눌 수 있음
-        System.out.println("3 / 0.0 = " + 3/0.0);
+        System.out.println("3 / 0.0 = " +  3/0.0);
+//        System.out.println("3 / 0 = " +  3/0); <- 얘는 오류
     }
 }
