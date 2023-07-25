2ba4e2c34fc01d5eb36c1eceb798698435c95447
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-07-25 23:51:09 +0900

    TvTest

---
+++ 예제/6_객체지향프로그래밍1/TvTest.java
@@ -0,0 +1,22 @@
+class Tv {
+    // Tv 속성(멤버 번수)
+    String color;
+    boolean power;
+    int channel;
+
+    // Tv의 기능(메서드)
+    void power(){power = !power; } // TV를 켜거나 끄는 기능을 하는 메서드
+    void channelUp(){ ++channel;} // TV의 채널을 높이는 기능을 하는 메서드
+    void channelDown(){--channel;} // TV의 채널을 낮추는 기능을 하는 메서드
+
+}
+
+class TvTest{
+    public static void main(String[] args) {
+        Tv t; //Tv 인스턴스를 참조하기 위한 변수 t를 선언
+        t = new Tv(); // Tv 인스턴스를 생성한다.
+        t.channel = 7; // Tv 인스턴스의 멤버 변수 channel의 값을 7로함
+        t.channelDown(); // Tv 인스턴스의 메서드 channelDown()을 호출
+        System.out.println("현재 채널은 "+ t.channel +"입니다.");
+    }
+}
