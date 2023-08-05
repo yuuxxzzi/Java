7c9b81a8c315e010805d3637806cca47b9fb2e10
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-08-05 22:12:33 +0900

    7_4

---
+++ 예제/7_객체지향프로그래밍2/TVCR.java
@@ -0,0 +1,34 @@
+class TV{
+    boolean power;
+    int channel;
+
+    void power(){ power = !power; }
+    void channelUp(){ ++channel; }
+    void channelDown() { --channel; }
+}
+class VCR{
+    boolean power;
+    int counter = 0;
+    void power(){ power = !power; }
+    void play(){/*내용 생략*/}
+    void stop(){/*내용 생략*/}
+    void rew(){/*내용 생략*/}
+    void ff(){/*내용 생략*/}
+
+}
+public class TVCR extends TV{
+    VCR vcr = new VCR();
+
+    void play(){
+        vcr.play();
+    }
+    void stop(){
+        vcr.stop();
+    }
+    void rew(){
+        vcr.rew();
+    }
+    void ff(){
+        vcr.ff();
+    }
+}
