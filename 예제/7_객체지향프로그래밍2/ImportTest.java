4ea0eba01f5421446bc2da36e458c93a8d285ea4
yuuxxzi <dbswl4282@g.hongik.ac.kr>
2023-08-05 23:44:42 +0900

    7_10

---
+++ 예제/7_객체지향프로그래밍2/ImportTest.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class ImportTest {
+    public static void main(String[] args) {
+        Date today = new Date();
+
+        SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat time = new SimpleDateFormat("hh:mm:ss a");
+
+        System.out.println("오늘 날씨는 " + date.format(today));
+        System.out.println("현재 날씨는 " + time.format(today));
+    }
+}
